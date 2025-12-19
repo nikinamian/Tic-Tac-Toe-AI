b375ab1bb2866378a03428bc332a336c20572e46
Niki Namian <nikinamian@gmail.com>
2025-12-19 15:33:56 -0800

    organizing src folders

---
+++ build/CMakeFiles/TicTacToeGame.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nikinamian/Desktop/Niki Namian Portfolio/Tic-Tac-Toe-AI--1/src/AI.cpp" "CMakeFiles/TicTacToeGame.dir/src/AI.cpp.o" "gcc" "CMakeFiles/TicTacToeGame.dir/src/AI.cpp.o.d"
+  "/Users/nikinamian/Desktop/Niki Namian Portfolio/Tic-Tac-Toe-AI--1/src/Board.cpp" "CMakeFiles/TicTacToeGame.dir/src/Board.cpp.o" "gcc" "CMakeFiles/TicTacToeGame.dir/src/Board.cpp.o.d"
+  "/Users/nikinamian/Desktop/Niki Namian Portfolio/Tic-Tac-Toe-AI--1/src/main.cpp" "CMakeFiles/TicTacToeGame.dir/src/main.cpp.o" "gcc" "CMakeFiles/TicTacToeGame.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TicTacToeGame.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TicTacToeGame.dir/src/AI.cpp.o"
+  "CMakeFiles/TicTacToeGame.dir/src/AI.cpp.o.d"
+  "CMakeFiles/TicTacToeGame.dir/src/Board.cpp.o"
+  "CMakeFiles/TicTacToeGame.dir/src/Board.cpp.o.d"
+  "CMakeFiles/TicTacToeGame.dir/src/main.cpp.o"
+  "CMakeFiles/TicTacToeGame.dir/src/main.cpp.o.d"
+  "TicTacToeGame"
+  "TicTacToeGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TicTacToeGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
